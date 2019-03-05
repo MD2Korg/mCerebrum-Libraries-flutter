f8500170df6e3c2dfeafdde118743ab3593b005a
monowar <monowar.hossain@gmail.com>
2019-03-04 19:44:41 -0600

    initial commit

---
+++ libraries/extensionapi/lib/extensionapi.dart
@@ -0,0 +1,34 @@
+library extensionapi;
+import 'package:flutter/material.dart';
+
+
+
+class MCExtensionAPI {
+  String id;
+  String title;
+  String description;
+  int versionCode;
+  String versionName;
+  List<String> permissionList;
+  List<UserInterface> userInterfaces;
+  MCExtensionAPI(this.id, {this.title, this.description, this.versionCode, this.versionName, this.permissionList, this.userInterfaces});
+//Bitmap icon;
+}
+class UserInterface {
+  String id;
+  String title;
+  String description;
+  Widget widget;
+  UserInterface(this.id,{this.title, this.description, this.widget});
+}
+
+class Configuration{
+  Function getConfigurationState;
+  Function setConfiguration;
+}
+class Action{
+  String id;
+  String title;
+  String description;
+  Function mcAction;
+}
