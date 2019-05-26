d8b6ec9eb4720c6a31427ede2cafca674a97c6d7
monowar <monowar.hossain@gmail.com>
2019-05-26 04:33:38 -0500

    initial commit

--- libraries/mcerebrumapi/lib/extensionapi.dart
+++ libraries/mcerebrumapi/lib/extensionapi.dart
@@ -2,40 +2,15 @@ library extensionapi;
 import 'package:flutter/material.dart';
 
 class MCExtensionAPI {
-  String id;
-  String title;
-  String description;
-  int versionCode;
-  String versionName;
-  List<String> permissionList;
-  List<UserInterface> userInterfaces;
-  MCExtensionAPI(this.id, {this.title, this.description, this.versionCode, this.versionName, this.permissionList, this.userInterfaces});
-  UserInterface getUserInterface(String id){
-    if(userInterfaces==null) return null;
-    for(int i=0;i<userInterfaces.length;i++){
-      if(userInterfaces[i].id==id){
-        return userInterfaces[i];
-      }
-    }
-    return null;
+  List<UserInterface> _userInterfaces;
+  MCExtensionAPI(List<UserInterface> userInterfaces){
+    this._userInterfaces = userInterfaces;
   }
-//Bitmap icon;
+
+  List<UserInterface> get userInterfaces => _userInterfaces;
+
 }
 class UserInterface{
-  final String id;
-  final String title;
-  final String description;
-  final Widget Function(Map<String, dynamic>) widget;
-  UserInterface(this.id,{this.title, this.description, this.widget});
-}
-
-class Configuration{
-  Function getConfigurationState;
-  Function setConfiguration;
-}
-class Action{
-  String id;
-  String title;
-  String description;
-  Function mcAction;
+  final Widget Function() widget;
+  UserInterface(this.widget);
 }
