8017eb040529b6ce026c368d9898ecb416be7054
monowar <monowar.hossain@gmail.com>
2019-05-27 00:51:30 -0500

    initial commit

--- libraries/mcerebrumapi/lib/extensionapi.dart
+++ libraries/mcerebrumapi/lib/extensionapi.dart
@@ -9,7 +9,7 @@ class MCExtensionAPI {
   Future<int> settingsStatus;
   Widget settingUI;
   Widget mainUI;
-  Map<String, Future<dynamic> Function({Map<String, dynamic> defaultConfig, Map<String, dynamic> config, Map<String, dynamic> params})> actions;
+  Map<String, Future<dynamic> Function(Map<String, dynamic> defaultConfig, Map<String, dynamic> config, Map<String, dynamic> params)> actions;
 
   MCExtensionAPI(this.id, {this.mainUI, this.userInterfaces, this.permissions, this.settingsStatus, this.settingUI, this.actions});
 }
