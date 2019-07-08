096bd5ee1f3c232c9c6f3151567c892a54666b43
monowar <monowar.hossain@gmail.com>
2019-07-08 16:18:53 -0500

    permission-handler library removed

--- libraries/mcerebrumapi/lib/extensionapi.dart
+++ libraries/mcerebrumapi/lib/extensionapi.dart
@@ -1,15 +1,13 @@
 library extensionapi;
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class MCExtensionAPI {
   String id;
   Map<String, WidgetBuilder> userInterfaces;
-  List<PermissionGroup> permissions;
   Future<int> settingsStatus;
   WidgetBuilder settingUI;
   WidgetBuilder mainUI;
   Map<String, Future<dynamic> Function(dynamic param)> actions;
 
-  MCExtensionAPI(this.id, {this.mainUI, this.userInterfaces, this.permissions, this.settingsStatus, this.settingUI, this.actions});
+  MCExtensionAPI(this.id, {this.mainUI, this.userInterfaces, this.settingsStatus, this.settingUI, this.actions});
 }
